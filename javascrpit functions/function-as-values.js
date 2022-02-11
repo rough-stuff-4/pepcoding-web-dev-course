e41a167f1afa96a77c81046f674eda2018e74e2f
bhagwati prasad <bhagwatijoshi325@gmail.com>
2022-02-11 11:55:53 +0530

    this code prints text and html of last ball commentry from a site

--- javascrpit functions/function-as-values.js
+++
@@ -1,6 +0,0 @@
-// FUNCTIOIN CAN BE USED AS VALUES
-function f_as_value (){
-    return 6;
-}
-let x =  f_as_value() * 7;
-console.log (x);
--- javascrpit functions/toString_on_functions.js
+++
@@ -1,6 +0,0 @@
-function func1(){
-    let i =0;
-    return i;
-}
-let func_definition_string = func1.toString();
-console.log(func_definition_string);
