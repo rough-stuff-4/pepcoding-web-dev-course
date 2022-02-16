767c2403cc6d8ae0c8bdb1f368917d1886d247b2
bhagwati prasad <bhagwatijoshi325@gmail.com>
2022-02-16 16:04:30 +0530

    still din't reach to an perfect procedure of (n^1.5)/36 time complexity solution

--- print_prime_till_N.js
+++ print_prime_till_N.js
@@ -42,13 +42,18 @@ function solve(low , high){
 
 
 function solve2(l, h){
-    let il = l/6 , til = l/6;
-    let ih = h/6 , tih = l/6;
+    let il = Math.floor(l/6) ,til = il;
+    let ih = Math.floor(h/6) , tih = ih;
     let rl = l%6 , rh = h%6;
     let flag2 = false;
-    
+    if (l<=2){
+        console.log(2);
+        console.log(3);
+    } else if (l<=3){
+        console.log(3);
+    }
     if(rl==1 || rl==0){ //so here 1st we check if (6*(il)+1) is a prime or not , than il++;
-        if(isPrime(6*(il)+1)) console.log(6*(il)+1);
+        if(isPrime((6*il)+1)) console.log(6*(il)+1);
         il++;
     } 
     // else if(rl == 0){ //here we only need to check 6*il+1 , than  il++;
@@ -56,23 +61,22 @@ function solve2(l, h){
     //     il++;
     // }
     
-    if(rh==5){ //so here 1st we check if (6*(ih+1)-1) is a prime or not , than ih--;
-        ih--;
+    if(rh==5){ //so here 1st we check if (6*(ih+1)-1) is a prime or not;
         flag2 = 1;
     } else if(rh == 0){  //here we only need to check 6*ih-1 , than decrease ih--;
         ih--;
         flag2 = 2;
     }
     
-    for(let i=il; i<=ih; i=+6){
+    for(let i=il; i<=ih; i++){
         if(isPrime((i*6)-1)) console.log((i*6)-1);
         if(isPrime((i*6)+1)) console.log((i*6)+1);
     }
     if(flag2 == 1) {
-        if(isPrime(6*(tih+1)-1)) console.log(6*(tih+1)-1);
+        if(isPrime((6*(tih+1))-1)) console.log((6*(tih+1))-1);
     }
     if(flag2 == 2) {
-        if(isPrime(6*(tih)-1)) console.log(6*(tih+1)-1);
+        if(isPrime(6*(tih)-1)) console.log(6*(tih)-1);
     }
 
 
@@ -80,11 +84,12 @@ function solve2(l, h){
 
 function isPrime(num){
     if (num == 2 || num == 3) return true;
-    else if ( num%2 == 0 || num%3 == 0) return false;
+    else if ( num%2 == 0 || num%3 == 0 || num < 2) return false;
     for(let i=6; i*i<=num; i+=6){
         if(num%(i-1) == 0 ||  num%(i+1) == 0 ) return false;
     }
     return true
 }
 
-solve2(6,24);
+solve2(-1,-1);
+
